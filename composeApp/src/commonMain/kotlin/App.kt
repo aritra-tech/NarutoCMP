f6aaf8444b499d837045f40c58a9f4c60216cd2e
aritra <aritrarick2002@gmail.com>
2024-01-28 22:02:32 +0530

    ✅ Transfer ViewModel to the `App` class

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,16 @@
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 import ui.screen.home.HomeScreen
 import ui.theme.NarutoCMPTheme
+import viewmodel.NarutoViewModel
 
 @Composable
 fun App() {
     NarutoCMPTheme {
-        Navigator(HomeScreen())
+        val narutoViewModel = getViewModel(Unit, viewModelFactory { NarutoViewModel() })
+
+        Navigator(HomeScreen(narutoViewModel))
     }
 }
