03f6b46ed4f85c3663054a92b5aed8b6f5e7d683
aritra <aritrarick2002@gmail.com>
2024-02-01 19:34:50 +0530

    Fixed scrolling issue of `Home Screen` for desktop

--- composeApp/src/commonMain/kotlin/ui/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/home/HomeScreen.kt
@@ -1,12 +1,17 @@
 package ui.screen.home
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lightbulb
 import androidx.compose.material.icons.outlined.Lightbulb
@@ -20,6 +25,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -29,6 +35,7 @@ import cafe.adriel.voyager.navigator.currentOrThrow
 import component.CharacterCard
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
+import kotlinx.coroutines.launch
 import ui.screen.details.CharacterDetailsScreen
 import ui.theme.LocalUiModePreferenceController
 import ui.theme.UiMode
@@ -45,6 +52,8 @@ class HomeScreen : Screen {
         val characterUiStateObserve by narutoViewModel.narutoUiState.collectAsState()
         val controller = LocalUiModePreferenceController.current
         val uiMode by rememberUiMode()
+        val scrollState = rememberLazyGridState()
+        val scope = rememberCoroutineScope()
 
         Scaffold(
             topBar = {
@@ -67,13 +76,22 @@ class HomeScreen : Screen {
         ) {
             AnimatedVisibility(characterUiStateObserve.naruto.characters.isNotEmpty()) {
                 LazyVerticalGrid(
+                    state = scrollState,
                     columns = GridCells.Fixed(2),
                     horizontalArrangement = Arrangement.spacedBy(10.dp),
                     verticalArrangement = Arrangement.spacedBy(10.dp),
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(it)
-                        .padding(horizontal = 16.dp, vertical = 20.dp),
+                        .padding(horizontal = 16.dp, vertical = 20.dp)
+                        .draggable(
+                            orientation = Orientation.Horizontal,
+                            state = rememberDraggableState { delta ->
+                                scope.launch {
+                                    scrollState.scrollBy(-delta)
+                                }
+                            }
+                        ),
                     content = {
                         items(characterUiStateObserve.naruto.characters) { narutoCharacter ->
                             CharacterCard(
